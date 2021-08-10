3592cc7804baf50eba7f924b58aef1ff30ec09ee
Basem Elgendy <basem_elgendy@2grand.net>
2021-08-10 19:53:21 +0200

    Initial Commit

---
+++ BGApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BGApp
+//
+//  Created by BasemElgendy on 10/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
